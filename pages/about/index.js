5e3ac954f7b9df412e4ac830d80dfb0d8ec55561
Guillaume Rossi <guillaum.ro@gmail.com>
2019-10-13 21:51:59 +0200

    [WIP] slider can move by draging or clicking

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,8 +5,8 @@ const AboutIndex = () => {
         <div className="row">
           <div className="main col-8 offset-2">
             <h1>About this</h1>
-            <p>Just a small project I made to improve my widget coding skills</p>
-            <p>Made with <img src="https://img.icons8.com/officel/16/000000/filled-like.png" /> and <a href="https://nextjs.org/">Next.js</a></p>
+            <p>Just a small project I made to improve my widget coding skills<br/>
+            Made with <img src="https://img.icons8.com/officel/16/000000/filled-like.png" /> and <a href="https://nextjs.org/">Next.js</a></p>
             <a href="https://github.com/GuillRo/widgets-showcase"><img src="https://img.icons8.com/ios/50/000000/github.png" /></a>
           </div>
         </div>
@@ -22,6 +22,14 @@ const AboutIndex = () => {
           color: black;
           text-decoration: none;
           font-weight: bold;
+        }
+        h1 {
+          margin-top: 45px;
+        }
+        p {
+          margin-top: 25px;
+          line-height: 35px;
+          font-size: 18px;
         }
       `}</style>
     </>
